17f8f86493115ceeb6f1c136daff54c221429e4c
Katsuro Kurosaki <kurosaki@katsunet.com>
2016-12-23 17:56:45 +0100

    Implemented move to auth server if some other ones stop or crash

--- src/com/katsunet/bungee/evts/ServerKickEvt.java
+++ src/com/katsunet/bungee/evts/ServerKickEvt.java
@@ -1,8 +1,10 @@
 package com.katsunet.bungee.evts;
 
+import com.katsunet.common.Global;
 import com.katsunet.spkproxysuite.bungee.Main;
 
-import net.md_5.bungee.api.event.PermissionCheckEvent;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -17,9 +19,11 @@ public class ServerKickEvt implements Listener {
 	
 	@EventHandler
 	public void onServerKickEvent(ServerKickEvent e){
-		System.out.println("Server kick");
-		System.out.println(e.toString());
-		
+		if(!e.getKickedFrom().getName().equals(this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_AUTHLOBBY_SERVER))){
+			e.setCancelled(true);
+			e.getPlayer().sendMessage(new TextComponent("La modalidad ha sido apagada. Usa /"+this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER)+" para volver a entrar."));
+			e.getPlayer().connect(ProxyServer.getInstance().getServerInfo(this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_AUTHLOBBY_SERVER)));
+		}
 	}
 
 }
