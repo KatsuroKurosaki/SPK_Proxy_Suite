600adbb62c13fd37644e268abef690d5e6808e46
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-01-08 00:18:18 +0100

    Removed command functionality

--- src/com/katsunet/bungee/cmds/info/ICmd.java
+++ src/com/katsunet/bungee/cmds/info/ICmd.java
@@ -4,7 +4,6 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
@@ -24,18 +23,12 @@ public class ICmd extends Command {
 					for (int i = 1; i < arg1.length; i++) {
 						builder.append(" " + arg1[i]);
 					}
-					player.sendMessage(new TextComponent(ChatColor.RED + "*******************************"));		
-					player.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&',builder.toString())));		
-					player.sendMessage(new TextComponent(ChatColor.RED + "*******************************"));
-					player.sendMessage(ChatMessageType.ACTION_BAR,new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', builder.toString())).create());					
+					player.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', builder.toString())));
+					player.sendMessage(ChatMessageType.ACTION_BAR,new TextComponent(ChatColor.translateAlternateColorCodes('&', builder.toString())));
 					builder = null;
 					player = null;
 				}
 			}
-		} else {
-			ProxiedPlayer player = (ProxiedPlayer) arg0;
-			player.disconnect(new TextComponent("Comando de desconexión para vagos."));
-			player = null;
 		}
 	}
 }
