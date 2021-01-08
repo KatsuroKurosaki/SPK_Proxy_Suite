21d95e22ef261883ab31cbe29693b6a5a9fd3cfb
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-01-09 00:47:35 +0100

    COde format and translated bgc command

--- src/com/katsunet/bungee/cmds/bgc/BgcCmd.java
+++ src/com/katsunet/bungee/cmds/bgc/BgcCmd.java
@@ -6,26 +6,25 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
 
-public class BgcCmd extends Command{
+public class BgcCmd extends Command {
 
-	public BgcCmd(){
-		super("bgc",Global.PERM_BGCVIEW);
+	public BgcCmd() {
+		super("bgc", Global.PERM_BGCVIEW);
 	}
-	
+
 	@Override
 	public void execute(CommandSender arg0, String[] arg1) {
-		if(arg1.length==0){
+		if (arg1.length == 0) {
 			this.gcCall(arg0);
 		} else {
-			if(arg1[0].equals("free")){
-				if(arg0.hasPermission(Global.PERM_BGCFREE)){
-					arg0.sendMessage(new TextComponent("[§bGC§f] Actualmente..."));
+			if (arg1[0].equals("free")) {
+				if (arg0.hasPermission(Global.PERM_BGCFREE)) {
 					this.gcCall(arg0);
-			        arg0.sendMessage(new TextComponent("[§bGC§f] Realizando limpieza..."));
-			        System.gc();
-			        this.gcCall(arg0);
+					arg0.sendMessage(new TextComponent("After RAM cleanup"));
+					System.gc();
+					this.gcCall(arg0);
 				} else {
-					arg0.sendMessage(new TextComponent("[§bGC§f] No tienes permiso para limpiar la memoria del server."));
+					arg0.sendMessage(new TextComponent("No permission to clear server RAM."));
 				}
 			} else {
 				this.gcCall(arg0);
@@ -33,13 +32,12 @@ public class BgcCmd extends Command{
 		}
 	}
 
-	
-	private void gcCall(CommandSender sender){
-		int freeMemory = (int)(Runtime.getRuntime().freeMemory() / 1000000L);
-		int maxMemory = (int)(Runtime.getRuntime().maxMemory() / 1000000L);
-		sender.sendMessage(new TextComponent("Uso de RAM:"));
-		sender.sendMessage(new TextComponent("RAM libre: " + freeMemory + " MB."));
-		sender.sendMessage(new TextComponent("RAM maxima: " + maxMemory + " MB."));
-		sender.sendMessage(new TextComponent("Uso de RAM actual: " + (maxMemory - freeMemory) + " MB."));
+	private void gcCall(CommandSender sender) {
+		int freeMemory = (int) (Runtime.getRuntime().freeMemory() / 1000000L);
+		int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1000000L);
+		sender.sendMessage(new TextComponent("RAM usage:"));
+		sender.sendMessage(new TextComponent("Free: " + freeMemory + " MB."));
+		sender.sendMessage(new TextComponent("Maximum: " + maxMemory + " MB."));
+		sender.sendMessage(new TextComponent("Current: " + (maxMemory - freeMemory) + " MB."));
 	}
 }
