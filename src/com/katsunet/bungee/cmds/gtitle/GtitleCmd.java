e708b3b65482d69cd979542d136c9a5d66d8cd29
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-01-08 18:08:24 +0100

    Code format

--- src/com/katsunet/bungee/cmds/gtitle/GtitleCmd.java
+++ src/com/katsunet/bungee/cmds/gtitle/GtitleCmd.java
@@ -13,40 +13,42 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.Title;
 
 public class GtitleCmd extends Command {
-	
+
 	private Main _plugin;
-	
-	public GtitleCmd(Main plugin){
-		super("gtitle",Global.PERM_GTITLE_COMMAND,new String[]{"gt"});
-		this._plugin=plugin;
+
+	public GtitleCmd(Main plugin) {
+		super("gtitle", Global.PERM_GTITLE_COMMAND, new String[] { "gt" });
+		this._plugin = plugin;
 	}
 
 	@Override
 	public void execute(CommandSender arg0, String[] arg1) {
-		if(arg1.length>0){
+		if (arg1.length > 0) {
 			StringBuilder strb = new StringBuilder();
-			for(int i=0;i<arg1.length;i++) {
-				strb.append(" "+arg1[i]);
+			for (int i = 0; i < arg1.length; i++) {
+				strb.append(" " + arg1[i]);
 			}
 			String[] message = strb.toString().split("|");
-			
+
 			Title titleToPlayer = ProxyServer.getInstance().createTitle()
-				.title(new ComponentBuilder( ChatColor.translateAlternateColorCodes('&',message[0]) ).create() );
-			if(message.length>1){
-				titleToPlayer.subTitle(new ComponentBuilder( ChatColor.translateAlternateColorCodes('&',message[1]) ).create() );
+					.title(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', message[0])).create());
+			if (message.length > 1) {
+				titleToPlayer.subTitle(
+						new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', message[1])).create());
 			}
-			titleToPlayer.fadeIn(this._plugin.getMainCnf().getYaml().getInt(Global.CONFNODE_GTITLE_FADE)*20)
-				.stay(this._plugin.getMainCnf().getYaml().getInt(Global.CONFNODE_GTITLE_STAY)*20)
-				.fadeOut(this._plugin.getMainCnf().getYaml().getInt(Global.CONFNODE_GTITLE_FADE)*20);
-			
+			titleToPlayer.fadeIn(this._plugin.getMainCnf().getYaml().getInt(Global.CONFNODE_GTITLE_FADE) * 20)
+					.stay(this._plugin.getMainCnf().getYaml().getInt(Global.CONFNODE_GTITLE_STAY) * 20)
+					.fadeOut(this._plugin.getMainCnf().getYaml().getInt(Global.CONFNODE_GTITLE_FADE) * 20);
+
 			for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
 				titleToPlayer.send(player);
 			}
-			
-			strb=null;
-			message=null;
+
+			strb = null;
+			message = null;
 		} else {
-			arg0.sendMessage(new TextComponent("[§bGTitle§f] No has escrito texto a enviar. Uso: /gt <titulo>//<subtitulo>."));
+			arg0.sendMessage(
+					new TextComponent("[ï¿½bGTitleï¿½f] No has escrito texto a enviar. Uso: /gt <titulo>//<subtitulo>."));
 		}
 	}
 }
