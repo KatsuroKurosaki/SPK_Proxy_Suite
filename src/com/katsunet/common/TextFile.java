ecda7d0985f2effb99ebaa3cd5b7b09bb2b9a97b
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-01-08 15:57:20 +0100

    Use awesome Java7 Files class

--- src/com/katsunet/common/TextFile.java
+++ src/com/katsunet/common/TextFile.java
@@ -1,36 +1,35 @@
 package com.katsunet.common;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class TextFile {
 
 	private Plugin plugin;
-	private File file;
+	private String fileName;
 
 	public TextFile(Plugin plugin, String fileName) {
 		this.plugin = plugin;
 		if (!this.plugin.getDataFolder().exists()) {
 			this.plugin.getDataFolder().mkdir();
 		}
-		this.file = new File(this.plugin.getDataFolder().getPath() + File.separator + fileName);
-		if (!this.file.exists()) {
+		this.fileName = this.plugin.getDataFolder().getPath() + File.separator + fileName;
+		File f = new File(this.fileName);
+		if (!f.exists()) {
 			this.plugin.getLogger().info("Error 404: " + fileName + " Not Found! Creating empty file.");
-			this.createTextFile();
+			this.createTextFile(f);
 		}
+		f = null;
 	}
 
-	private void createTextFile() {
+	private void createTextFile(File f) {
 		try {
-			if (this.file.createNewFile()) {
-				System.out.println("File created: " + this.file.getName());
-			} else {
-				System.out.println("File already exists.");
+			if (f.createNewFile()) {
+				System.out.println("File created: " + f.getName());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -38,34 +37,19 @@ public class TextFile {
 	}
 
 	public String readTextFile() {
-		String fileContents = null;
 		try {
-			Scanner reader = new Scanner(this.file);
-			StringBuilder builder = new StringBuilder();
-			while (reader.hasNextLine()) {
-				builder.append(reader.nextLine());
-			}
-			fileContents = builder.toString();
-			builder = null;
-			reader.close();
-			reader = null;
-		} catch (FileNotFoundException e) {
+			return new String(Files.readAllBytes(Paths.get(this.fileName)));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return fileContents;
+		return "";
 	}
 
 	public void writeTextFile(String fileContents) {
 		try {
-			FileWriter writer = new FileWriter(this.file);
-			writer.write(fileContents);
-			writer.close();
+			Files.write(Paths.get(this.fileName), fileContents.getBytes());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public File getFile() {
-		return this.file;
 	}
 }
