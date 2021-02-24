968e722e350804705d7cda32f99d8fe9ee66424c
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-02-24 17:02:15 +0100

    Lobby command complete

--- src/com/katsunet/bungee/cmds/lobby/LobbyCmd.java
+++ src/com/katsunet/bungee/cmds/lobby/LobbyCmd.java
@@ -9,30 +9,44 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-public class LobbyCmd extends Command{
+public class LobbyCmd extends Command {
 
 	private Main plugin;
-	
-	public LobbyCmd(Main plugin){
-		super(plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_COMMAND),"",plugin.getMainCnf().getYaml().getStringList(Global.CONFNODE_LOBBY_COMMANDALIAS).stream().toArray(String[]::new));
+
+	public LobbyCmd(Main plugin) {
+		super(
+			plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_COMMAND),
+			"",
+			plugin.getMainCnf().getYaml().getStringList(Global.CONFNODE_LOBBY_COMMANDALIAS).stream().toArray(String[]::new)
+		);
 		this.plugin = plugin;
 	}
-	
+
 	@Override
 	public void execute(CommandSender arg0, String[] arg1) {
-		if(arg0 instanceof ProxiedPlayer)	{
-			ProxiedPlayer p = (ProxiedPlayer)arg0;
-			if (!p.getServer().getInfo().getName().equals(this.plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER))){
+		if (arg0 instanceof ProxiedPlayer) {
+			ProxiedPlayer p = (ProxiedPlayer) arg0;
+			if (!p.getServer().getInfo().getName().equals(this.plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER))) {
 				if (ProxyServer.getInstance().getServers().containsKey(this.plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER))) {
-					p.connect(ProxyServer.getInstance().getServerInfo(this.plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER)));
+					p.connect(
+						ProxyServer.getInstance().getServerInfo(
+							this.plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER)
+						)
+					);
 				} else {
-					p.sendMessage(new TextComponent("[§bLobby§f] Error: No puedo encontrar el lobby. Contacta al Admin."));
+					p.sendMessage(
+						new TextComponent(
+							"Internal error connecting to the lobby. Contact the admin."
+						)
+					);
 				}
 			} else {
-				p.sendMessage(new TextComponent("[§bLobby§f] Ya estás conectado al lobby."));
+				p.sendMessage(
+					new TextComponent(
+						"You are already connected to the lobby."
+					)
+				);
 			}
-		} else {
-			arg0.sendMessage(new TextComponent("Console-tan no puede ir al lobby."));
 		}
 	}
 
