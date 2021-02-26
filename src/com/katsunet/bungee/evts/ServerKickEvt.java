569b4c7b6baa328503cd2181d82b651cded39643
Joaquim Sardiña <joaquimsb89@gmail.com>
2021-02-26 10:40:27 +0100

    Return users to lobby if other server goes down, and kick them if lobby is down.

--- src/com/katsunet/bungee/evts/ServerKickEvt.java
+++ src/com/katsunet/bungee/evts/ServerKickEvt.java
@@ -10,20 +10,25 @@ import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class ServerKickEvt implements Listener {
-	
-	private Main _plugin;
-	
-	public ServerKickEvt(Main plugin){
-		this._plugin=plugin;
-	}
-	
-	@EventHandler
-	public void onServerKickEvent(ServerKickEvent e){
-		if(!e.getKickedFrom().getName().equals(this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_AUTHLOBBY_SERVER))){
-			e.setCancelled(true);
-			e.getPlayer().sendMessage(new TextComponent("La modalidad ha sido apagada. Usa /"+this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER)+" para volver a entrar."));
-			e.getPlayer().connect(ProxyServer.getInstance().getServerInfo(this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_AUTHLOBBY_SERVER)));
-		}
-	}
+
+  private Main _plugin;
+
+  public ServerKickEvt(Main plugin) {
+    this._plugin = plugin;
+  }
+
+  @EventHandler
+  public void onServerKickEvent(ServerKickEvent e) {
+    if (!e.getKickedFrom().getName()
+        .equals(this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_AUTHLOBBY_SERVER))) {
+      e.setCancelled(true);
+      e.getPlayer()
+          .sendMessage(new TextComponent("The server was shutdown. Use /"
+              + this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_LOBBY_SERVER)
+              + " to return to the lobby."));
+      e.getPlayer().connect(ProxyServer.getInstance().getServerInfo(
+          this._plugin.getMainCnf().getYaml().getString(Global.CONFNODE_AUTHLOBBY_SERVER)));
+    }
+  }
 
 }
